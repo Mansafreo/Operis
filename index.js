852b21a1af834a1c56c3934e1c2ee8520073693d
Mansafreo <samoeicoder@gmail.com>
2024-04-16 22:15:32 +0300

    Created sidebar and navbar

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // main.js
 require('electron-reload')(__dirname);//To support hot-reloading for the UI
-
+const path=require('path');//To support the use of path.resolve
+let main_file=path.resolve(__dirname, 'UI', 'index.html');
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   // Create the browser window
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1980,
+    height: 1000,
+    fullscreen: false,//This is set to true to make the window full screen
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false//This is set to false to allow the use of require in the UI
@@ -15,7 +17,7 @@ function createWindow() {
   });
 
   // Load your HTML file or URL
-  mainWindow.loadFile('index.html');
+  mainWindow.loadFile(main_file);
 }
 
 // Create the main window when the app is ready
